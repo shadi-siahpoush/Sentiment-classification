a18b140a4a19ff0918516c218b08e515b8a349d7
shadi-siahpoush <shadi.siahpoush@gmail.com>
2021-04-27 19:09:54 -0700

    changed gitignore

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-sudo y| docker system prune -a
-sudo docker build -t test .
-sudo docker run -it test
+sudo y| docker system prune -a # removed all the images
+sudo docker build -t test . # build image from the dockerfile in the curent folder
+sudo docker image list # show if the image has been created
+sudo docker run -it test # run the image after it is created
